6f013d03a5930b54bf7174c52a915f85cad8b0a9
RomanVL <romanvanloo@icloud.com>
2019-03-04 15:08:23 +0100

    Cars index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(email: "tester@test.com", password: "test123", password_confirmation: "test123")
+
+10.times do |i|
+  car = Car.new
+  car.name = "Diego's ferrari #{i}"
+  car.description = "Amazing car #{i}"
+  car.location = "Brussels #{i}"
+  car.capacity = 2
+  car.price = 300
+  car.make = 'Ferrari'
+  car.model = 'Spider'
+  car.ac = true
+  car.fuel = "Diesel"
+  car.consumption = "10l/100km"
+  car.min_age = 25
+  car.year = 2018
+  car.kilometers = 102_938
+  car.user = user
+  car.save!
+end
