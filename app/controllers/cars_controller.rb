7fb394ddb99f58299ca49adc28dc6c28693068d2
Thierry <zthi@hotmail.com>
2019-03-05 12:17:57 +0100

    cloudinary

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,25 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
     @reviews = Review.where(car_id: @car.id)
   end
+# TZ added
+  def new
+    @car = Car.new
+  end
+
+    def create
+    @car = Car.new car_params
+    @car.user = current_user
+    if @car.save!
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+# TZ private
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :location, :description, :capacity, :price, :user_id, :make, :model, :ac, :fuel, :consumption, :min_age, :year, :kilometers, :photo, :photo_cache)
+  end
 end
+
