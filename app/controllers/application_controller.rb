cd298b1c7ad72a7dd56caded73b39fe69a81adc3
RomanVL <romanvanloo@icloud.com>
2019-03-05 10:36:30 +0100

    generated devise controller/views + routed logout => login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  # redirect to the desired page after sign out
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
