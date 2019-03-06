687bdc3d2721bc56cbe52d42e51f9614d33f739b
RomanVL <romanvanloo@icloud.com>
2019-03-06 14:43:38 +0100

    redirect logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   # redirect to the desired page after sign out
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    root_path
   end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
