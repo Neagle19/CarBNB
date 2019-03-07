720170cfe90aff0a52e0ee96f4b5dc1e21bc25e4
RomanVL <romanvanloo@icloud.com>
2019-03-07 11:21:31 +0100

    messages works except "joker"

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    messages = Message.where(sender: current_user)
+    messages += Message.where(receiver: current_user)
+    messages.sort_by! { |message| message.created_at }
+    @conversations_with = []
+    messages.each do |message|
+      if message.sender == current_user && !@conversations_with.include?([message.receiver, *, *])
+        @conversations_with.push([message.receiver, message.created_at, message.value])
+      elsif message.receiver == current_user && !@conversations_with.include?([message.sender, *, *])
+        @conversations_with.push([message.sender, message.created_at, message.value])
+      end
+    end
   end
 
   def show
@@ -8,21 +19,23 @@ class MessagesController < ApplicationController
     @messages_received = @sender.received_messages.where(sender: @receiver)
     @messages_sent = @sender.sent_messages.where(receiver: @receiver)
     @messages = @messages_sent + @messages_received
-    puts @messages
+    @messages.sort_by! { |message| message.created_at }
     @new_message = Message.new
   end
 
   def create
     @new_message = Message.new(check_message_params)
     @new_message.sender = current_user
-    puts "-"*60
-    puts @new_message.receiver
+    if @new_message.save
+      redirect_to message_path(@new_message.receiver.id)
+    else
+      redirect_to message_path(@new_message.receiver.id)
+    end
   end
 
-private
+  private
 
   def check_message_params
-    params.require(:message).permit(:content, :subject, :receiver_id, :receiver)
+    params.require(:message).permit(:content, :subject, :receiver_id)
   end
-
 end
